974799f5201a1a5eb8adb68418bb0f37f80b3c8d
marcus-zz <marcuszoldak@gmail.com>
2025-08-05 15:22:05 -0600

    Sage read these changes.
    I'm not sure what these changes to the code do/if they're necessary. Going to put this as a pull request for now so you can see if these changes are necessary.

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://thereefcraftproject.github.io/reefcraft.github.io/",
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
