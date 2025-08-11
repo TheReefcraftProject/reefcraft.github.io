b501471dde4d6af72c332b5c24b7c68d595c5762
marcus-zz <marcuszoldak@gmail.com>
2025-08-11 09:59:43 -0600

    Replace About background image with uploaded JPEG

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -89,7 +89,7 @@ const About = () => {
               <div className="relative h-96 md:h-full">
                 <div className="absolute inset-0 bg-gradient-to-br from-coral-400/20 to-teal-400/20"></div>
                 <img 
-                  src="https://images.pexels.com/photos/1076758/pexels-photo-1076758.jpeg?auto=compress&cs=tinysrgb&w=800" 
+                  src="./public/WhatsApp Image 2025-07-22 at 19.40.16.jpeg" 
                   alt="Vibrant coral reef ecosystem"
                   className="w-full h-full object-cover"
                 />
